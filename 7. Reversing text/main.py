78ec272035ad7c0bc2de209e1397a2ab090112db
BlushedNanis <luis_danieltg@outlook.com>
2024-06-04 00:00:39 -0400

    Added different solution

--- 7. Reversing text/main.py
+++ 7. Reversing text/main.py
@@ -1,5 +1,6 @@
 TEXT = "Hello World"
 
+# With for loop
 reversed_word = ""
 
 for letter in TEXT:
@@ -7,3 +8,7 @@ for letter in TEXT:
     
 print(reversed_word)
 
+# With slicing
+reversed_word2 = TEXT[::-1]
+
+print(reversed_word2)
